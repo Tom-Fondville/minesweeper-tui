cb55d6248383060a067ec0afc2041afe66229de9
t.fondville <t.fondville@betclicgroup.com>
2026-02-11 23:59:39 +0100

    first models and fixed board config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use minesweeper_tui::game::board::{Board, Difficulty};
+
 fn main() {
     println!("Hello, world!");
+    let board: Board = Board::new(Difficulty::Easy);
+    println!("board: {:?}", board);
 }
