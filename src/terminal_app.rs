808e28304893424bb1cdf23e6d860c003c2f62ce
t.fondville <t.fondville@betclicgroup.com>
2026-03-08 00:45:26 +0100

    yay it is working I can quit

--- src/terminal_app.rs
+++ src/terminal_app.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::io::{self};
 
 use crossterm::event::{self, Event, KeyCode, KeyEvent};
 use ratatui::DefaultTerminal;
@@ -16,27 +16,47 @@ impl TerminalApp {
 
     fn run(terminal: &mut DefaultTerminal, app: &mut App) -> io::Result<()> {
         loop {
-            let terminal_state: TerminalState = match app.current_state {
-                AppState::MainMenu => TerminalMainMenuState {},
-                AppState::Exiting => TerminalExitingState {},
-            };
-            Self::handle_tick(app, terminal_state, terminal);
+            // let terminal_state: TerminalState = match app.current_state {
+            //     AppState::MainMenu => TerminalMainMenuState {},
+            //     AppState::Exiting => TerminalExitingState {},
+            // };
+            // Self::handle_tick(app, terminal_state, terminal);
+
+            Self::handle_tick(app, terminal);
+
+            if app.exit {
+                break Ok(());
+            }
         }
     }
 
     fn handle_tick(
         app: &mut App,
-        terminal_state: impl TerminalState,
+        // terminal_state: impl TerminalState,
         terminal: &mut DefaultTerminal,
-    ) -> impl TerminalState {
-        terminal_state.draw(terminal, app);
+    ) {
+        // I can figure how to do generic things yet, so for the moment i will go with match
+        // statements and keep my Trait thing
+        // terminal_state.draw(terminal, app);
+        match app.current_state {
+            AppState::MainMenu => TerminalMainMenuState::draw(terminal, app),
+            AppState::Exiting => TerminalExitingState::draw(terminal, app),
+        }
+
         let event = event::read();
         if event.is_err() {
             panic!()
         }
-        match event.unwrap() {
-            Event::Key(key_event) => terminal_state.handle_key_event(terminal, app, key_event),
-            _ => (),
+
+        if let Event::Key(key_event) = event.unwrap() {
+            match app.current_state {
+                AppState::MainMenu => {
+                    TerminalMainMenuState::handle_key_event(terminal, app, key_event)
+                }
+                AppState::Exiting => {
+                    TerminalExitingState::handle_key_event(terminal, app, key_event)
+                }
+            }
         }
 
         //TODO: return the new state instead of this
@@ -44,27 +64,30 @@ impl TerminalApp {
         //     AppState::MainMenu => TerminalMainMenuState {},
         //     AppState::Exiting => TerminalExitingState {},
         // }
-        terminal_state
+        // terminal_state
     }
 }
 
 pub trait TerminalState {
-    fn draw(&self, terminal: &mut DefaultTerminal, app: &mut App);
-    fn handle_key_event(&self, terminal: &mut DefaultTerminal, app: &mut App, key_event: KeyEvent);
+    fn draw(terminal: &mut DefaultTerminal, app: &mut App);
+    fn handle_key_event(terminal: &mut DefaultTerminal, app: &mut App, key_event: KeyEvent);
+    // fn draw(&self, terminal: &mut DefaultTerminal, app: &mut App);
+    // fn handle_key_event(&self, terminal: &mut DefaultTerminal, app: &mut App, key_event: KeyEvent);
 }
 
 pub struct TerminalMainMenuState {}
 impl TerminalState for TerminalMainMenuState {
-    fn draw(&self, terminal: &mut DefaultTerminal, app: &mut App) {
-        terminal.draw(|frame| frame.render_widget("hello world", frame.area()));
+    fn draw(terminal: &mut DefaultTerminal, app: &mut App) {
+        let _ = terminal.draw(|frame| frame.render_widget("hello world", frame.area()));
     }
 
-    fn handle_key_event(&self, terminal: &mut DefaultTerminal, app: &mut App, key_event: KeyEvent) {
+    fn handle_key_event(terminal: &mut DefaultTerminal, app: &mut App, key_event: KeyEvent) {
         match key_event.kind {
-            event::KeyEventKind::Press => match key_event.code {
-                KeyCode::Char('q') => app.current_state = AppState::Exiting,
-                _ => (),
-            },
+            event::KeyEventKind::Press => {
+                if let KeyCode::Char('q') = key_event.code {
+                    app.current_state = AppState::Exiting
+                }
+            }
             event::KeyEventKind::Repeat => (),
             event::KeyEventKind::Release => (),
         }
@@ -73,16 +96,17 @@ impl TerminalState for TerminalMainMenuState {
 
 pub struct TerminalExitingState {}
 impl TerminalState for TerminalExitingState {
-    fn draw(&self, terminal: &mut DefaultTerminal, app: &mut App) {
+    fn draw(terminal: &mut DefaultTerminal, app: &mut App) {
         let _ = terminal.draw(|frame| {
             frame.render_widget("press enter if you realy want to quit", frame.area())
         });
     }
 
-    fn handle_key_event(&self, terminal: &mut DefaultTerminal, app: &mut App, key_event: KeyEvent) {
+    fn handle_key_event(terminal: &mut DefaultTerminal, app: &mut App, key_event: KeyEvent) {
         match key_event.kind {
             event::KeyEventKind::Press => match key_event.code {
                 KeyCode::Enter => app.exit = true,
+                // KeyCode::Enter => panic!(),
                 _ => app.current_state = AppState::MainMenu,
             },
             event::KeyEventKind::Repeat => (),
