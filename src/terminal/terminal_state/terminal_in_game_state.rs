8ead0c4810d2d643bc78b81a043afc18a3461a98
tomfondville <tomfondville@gmail.com>
2026-03-11 00:41:57 +0100

    on essaye de rendre ça joli

--- src/terminal/terminal_state/terminal_in_game_state.rs
+++ src/terminal/terminal_state/terminal_in_game_state.rs
@@ -1,12 +1,10 @@
-use std::ptr::read;
-
 use crossterm::event::{self, KeyCode, KeyEvent};
 use ratatui::{
-    DefaultTerminal,
-    layout::{Constraint, Direction, Layout},
+    DefaultTerminal, Frame,
+    layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Style, Stylize},
     text::Line,
-    widgets::Block,
+    widgets::Paragraph,
 };
 
 use crate::game::{
@@ -16,6 +14,51 @@ use crate::game::{
 
 pub struct TerminalInGameState {}
 impl TerminalInGameState {
+    pub fn draw_grid(frame: &mut Frame, area: Rect, board: &Board) {
+        let grid = board.get_grid();
+
+        let rows = grid.len() as u16;
+        let collumns = grid[0].len() as u16;
+        let cell_size = (area.height / rows).min(area.width / collumns);
+        let grid_width = cell_size * collumns;
+        let grid_height = cell_size * rows;
+
+        let grid_area = Rect {
+            x: area.x + (area.width - grid_width) / 2,
+            y: area.y + (area.height - grid_height) / 2,
+            width: grid_width,
+            height: grid_height,
+        };
+
+        let row_chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(vec![Constraint::Length(cell_size); rows as usize])
+            .split(grid_area);
+
+        for (y, cells_row) in grid.iter().enumerate() {
+            let col_chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints(vec![Constraint::Length(cell_size); collumns as usize])
+                .split(row_chunks[y]);
+
+            for (x, cell) in cells_row.iter().enumerate() {
+                let block_cell = match cell.cell_type {
+                    CellType::Bomb => Paragraph::new("*")
+                        .style(Style::default().bg(Color::Red).fg(Color::White))
+                        .centered(),
+                    CellType::Numbered(number) => Paragraph::new(number.to_string())
+                        .style(Style::default().bg(Color::DarkGray).fg(Color::Yellow))
+                        .centered(),
+                    CellType::Empty => Paragraph::new("")
+                        .style(Style::default().bg(Color::Gray))
+                        .centered(),
+                };
+
+                frame.render_widget(block_cell, col_chunks[x]);
+            }
+        }
+    }
+
     pub fn draw(terminal: &mut DefaultTerminal, board: &Board) {
         let _ = terminal.draw(|frame| {
             let chunks = Layout::default()
@@ -34,34 +77,38 @@ impl TerminalInGameState {
                 Line::from(format!(" {} ", board.get_difficulty().as_string().bold())).centered();
             frame.render_widget(title, header);
 
-            let board = board.get_grid();
-            let row_chunks = Layout::default()
-                .direction(Direction::Vertical)
-                .constraints(vec![Constraint::Length(1); board.len()])
-                .split(body);
+            TerminalInGameState::draw_grid(frame, body, board);
+            // let row_chunks = Layout::default()
+            //     .direction(Direction::Vertical)
+            //     .constraints(vec![Constraint::Ratio(1, grid.len() as u32); grid.len()])
+            //     .split(body);
+            //
+            // for (y, cells_row) in grid.iter().enumerate() {
+            //     let col_chunks = Layout::default()
+            //         .direction(Direction::Horizontal)
+            //         .constraints(vec![
+            //             Constraint::Ratio(1, cells_row.len() as u32);
+            //             cells_row.len()
+            //         ])
+            //         .split(row_chunks[y]);
+            //
+            //     for (x, cell) in cells_row.iter().enumerate() {
+            //         let block_cell = match cell.cell_type {
+            //             CellType::Bomb => Paragraph::new("*")
+            //                 .style(Style::default().bg(Color::Red).fg(Color::White))
+            //                 .centered(),
+            //             CellType::Numbered(number) => Paragraph::new(number.to_string())
+            //                 .style(Style::default().bg(Color::DarkGray).fg(Color::Yellow))
+            //                 .centered(),
+            //             CellType::Empty => Paragraph::new("")
+            //                 .style(Style::default().bg(Color::Gray))
+            //                 .centered(),
+            //         };
+            //
+            //         frame.render_widget(block_cell, col_chunks[x]);
+            //     }
+            // }
 
-            for (y, cells_row) in board.iter().enumerate() {
-                let col_chunks = Layout::default()
-                    .direction(Direction::Horizontal)
-                    .constraints(vec![Constraint::Length(2); cells_row.len()])
-                    .split(row_chunks[y]);
-
-                for (x, cell) in cells_row.iter().enumerate() {
-                    let block_cell = match cell.cell_type {
-                        CellType::Bomb => Block::default()
-                            .title("")
-                            .style(Style::default().bg(Color::Red).red()),
-                        CellType::Numbered(number) => Block::default()
-                            .title(number.to_string())
-                            .style(Style::default().bg(Color::Gray).black().bold()),
-                        CellType::Empty => Block::default()
-                            .title("")
-                            .style(Style::default().bg(Color::White).white()),
-                    };
-
-                    frame.render_widget(block_cell, col_chunks[x]);
-                }
-            }
             let footer_text = Line::from("minesweeper-tui".italic());
             frame.render_widget(footer_text, footer);
         });
