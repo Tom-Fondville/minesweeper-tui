2621352f1de0b70310c486525f0f0fbc43c2afc0
tomfondville <tomfondville@gmail.com>
2026-03-22 01:00:57 +0100

    refacto-ui wip

--- src/terminal/terminal_state/terminal_in_game_state.rs
+++ src/terminal/terminal_state/terminal_in_game_state.rs
@@ -1,24 +1,76 @@
 use crossterm::event::{self, KeyCode, KeyEvent};
 use ratatui::{
     DefaultTerminal,
-    buffer::Buffer,
-    layout::{Constraint, Direction, Layout, Rect},
-    style::{Color, Style, Stylize},
+    layout::{Constraint, Direction, Layout},
+    style::Stylize,
     text::Line,
-    widgets::{Block, Padding, Paragraph, Widget},
 };
 
-use crate::game::{
-    app::{App, AppState},
-    board::{Board, Cell, CellType},
+use crate::{
+    game::{
+        app::{App, AppState},
+        board::Board,
+    },
+    terminal::ui::board_ui::BoardUi,
 };
 
-const CELL_WIDTH: u16 = 7;
-const CELL_HEIGHT: u16 = 3;
+enum MoveDirection {
+    Up,
+    Down,
+    Right,
+    Left,
+}
 
-pub struct TerminalInGameState {}
-impl TerminalInGameState {
-    pub fn draw(terminal: &mut DefaultTerminal, board: &Board) {
+pub struct CursorPositon {
+    pub row: u16,
+    pub column: u16,
+}
+impl CursorPositon {
+    pub fn new(row: u16, column: u16) -> Self {
+        Self { row, column }
+    }
+}
+pub struct TerminalInGameState<'a> {
+    board: &'a Board,
+    cursor_position: CursorPositon,
+}
+
+impl<'a> TerminalInGameState<'a> {
+    pub fn new(board: &'a Board) -> Self {
+        Self {
+            board,
+            cursor_position: CursorPositon::new(0, 0),
+        }
+    }
+
+    fn move_cursor(mut self, direction: MoveDirection) {
+        match direction {
+            MoveDirection::Up => {
+                let new_row = u16::saturating_sub(self.cursor_position.row, 1);
+                self.cursor_position = CursorPositon::new(new_row, self.cursor_position.column);
+            }
+            MoveDirection::Down => {
+                let new_row = u16::saturating_add(self.cursor_position.row, 1);
+                if new_row > self.board.get_rows_count() - 1 {
+                    return;
+                }
+                self.cursor_position = CursorPositon::new(new_row, self.cursor_position.column);
+            }
+            MoveDirection::Right => {
+                let new_column = u16::saturating_add(self.cursor_position.column, 1);
+                if new_column > self.board.get_columns_count() + 1 {
+                    return;
+                }
+                self.cursor_position = CursorPositon::new(self.cursor_position.row, new_column);
+            }
+            MoveDirection::Left => {
+                let new_column = u16::saturating_sub(self.cursor_position.column, 1);
+                self.cursor_position = CursorPositon::new(self.cursor_position.row, new_column);
+            }
+        }
+    }
+
+    pub fn draw(self, terminal: &mut DefaultTerminal) {
         let _ = terminal.draw(|frame| {
             let chunks = Layout::default()
                 .direction(Direction::Vertical)
@@ -32,99 +84,32 @@ impl TerminalInGameState {
             let body = chunks[1];
             let footer = chunks[2];
 
-            let title =
-                Line::from(format!(" {} ", board.get_difficulty().as_string().bold())).centered();
+            let title = Line::from(format!(
+                " {} ",
+                self.board.get_difficulty().as_string().bold()
+            ))
+            .centered();
             frame.render_widget(title, header);
 
-            frame.render_widget(BoardUi::new(board), body);
+            frame.render_widget(BoardUi::new(self.board, &self.cursor_position), body);
 
             let footer_text = Line::from("minesweeper-tui".italic());
             frame.render_widget(footer_text, footer);
         });
     }
 
-    pub fn handle_key_event(app: &mut App, key_event: KeyEvent) {
+    pub fn handle_key_event(self, app: &mut App, key_event: KeyEvent) {
         match key_event.kind {
-            event::KeyEventKind::Press => {
-                if let KeyCode::Char('q') = key_event.code {
-                    app.change_current_state(AppState::Exiting(false));
-                }
-            }
+            event::KeyEventKind::Press => match key_event.code {
+                KeyCode::Char('q') => app.change_current_state(AppState::Exiting(false)),
+                KeyCode::Char('h') => self.move_cursor(MoveDirection::Left),
+                KeyCode::Char('j') => self.move_cursor(MoveDirection::Down),
+                KeyCode::Char('k') => self.move_cursor(MoveDirection::Up),
+                KeyCode::Char('l') => self.move_cursor(MoveDirection::Right),
+                _ => {}
+            },
             event::KeyEventKind::Repeat => (),
             event::KeyEventKind::Release => (),
         }
     }
-}
-
-pub struct BoardUi<'a> {
-    board: &'a Board,
-}
-
-impl<'a> BoardUi<'a> {
-    pub fn new(board: &'a Board) -> Self {
-        BoardUi { board }
-    }
-}
-
-impl<'a> Widget for BoardUi<'a> {
-    fn render(self, area: Rect, buf: &mut Buffer)
-    where
-        Self: Sized,
-    {
-        let grid = self.board.get_grid();
-
-        let rows = self.board.get_rows_count();
-        let collumns = self.board.get_columns_count();
-        let grid_width = CELL_WIDTH * collumns;
-        let grid_height = CELL_HEIGHT * rows;
-
-        let grid_area = Rect {
-            x: area.x + (area.width - grid_width) / 2,
-            y: area.y + (area.height - grid_height) / 2,
-            width: grid_width,
-            height: grid_height,
-        };
-
-        // here to debug the size of the grid
-        // frame.render_widget(Block::default().bg(Color::DarkGray), grid_area);
-        // return;
-
-        let row_chunks =
-            Layout::vertical(vec![Constraint::Length(CELL_HEIGHT); rows as usize]).split(grid_area);
-
-        for (y, cells_row) in grid.iter().enumerate() {
-            let col_chunks =
-                Layout::horizontal(vec![Constraint::Length(CELL_WIDTH); collumns as usize])
-                    .split(row_chunks[y]);
-
-            for (x, cell) in cells_row.iter().enumerate() {
-                render_cell(cell, col_chunks[x], buf);
-            }
-        }
-    }
-}
-
-fn render_cell(cell: &Cell, area: Rect, buf: &mut Buffer) {
-    match cell.cell_type {
-        CellType::Bomb => Paragraph::new("*")
-            .block(
-                Block::new()
-                    .style(Style::default().bg(Color::Red).fg(Color::White))
-                    .padding(Padding::top(1)),
-            )
-            .centered()
-            .render(area, buf),
-        CellType::Numbered(number) => Paragraph::new(number.to_string())
-            .block(
-                Block::new()
-                    .style(Style::default().bg(Color::DarkGray).fg(Color::Yellow))
-                    .padding(Padding::top(1)),
-            )
-            .centered()
-            .render(area, buf),
-        CellType::Empty => Block::new()
-            .on_black()
-            // .borders(Borders::all())
-            .render(area, buf),
-    };
 }
---
+++ src/terminal/ui/board_ui.rs
@@ -0,0 +1,96 @@
+use ratatui::{
+    buffer::Buffer,
+    layout::{Constraint, Layout, Rect},
+    style::{Color, Style},
+    widgets::{Block, Padding, Paragraph, Widget},
+};
+
+use crate::{
+    game::board::{Board, Cell, CellType},
+    terminal::terminal_state::terminal_in_game_state::CursorPositon,
+};
+
+const CELL_WIDTH: u16 = 7;
+const CELL_HEIGHT: u16 = 3;
+
+pub struct BoardUi<'a> {
+    board: &'a Board,
+    cursor_position: &'a CursorPositon,
+}
+
+impl<'a> BoardUi<'a> {
+    pub fn new(board: &'a Board, cursor_position: &'a CursorPositon) -> Self {
+        BoardUi {
+            board,
+            cursor_position,
+        }
+    }
+}
+
+impl<'a> Widget for BoardUi<'a> {
+    fn render(self, area: Rect, buf: &mut Buffer)
+    where
+        Self: Sized,
+    {
+        let grid = self.board.get_grid();
+
+        let rows = self.board.get_rows_count();
+        let collumns = self.board.get_columns_count();
+        let grid_width = CELL_WIDTH * collumns;
+        let grid_height = CELL_HEIGHT * rows;
+
+        let grid_area = Rect {
+            x: area.x + (area.width - grid_width) / 2,
+            y: area.y + (area.height - grid_height) / 2,
+            width: grid_width,
+            height: grid_height,
+        };
+
+        // here to debug the size of the grid
+        // frame.render_widget(Block::default().bg(Color::DarkGray), grid_area);
+        // return;
+
+        let row_chunks =
+            Layout::vertical(vec![Constraint::Length(CELL_HEIGHT); rows as usize]).split(grid_area);
+
+        for (y, cells_row) in grid.iter().enumerate() {
+            let col_chunks =
+                Layout::horizontal(vec![Constraint::Length(CELL_WIDTH); collumns as usize])
+                    .split(row_chunks[y]);
+
+            for (x, cell) in cells_row.iter().enumerate() {
+                if x == self.cursor_position.row.into() && y == self.cursor_position.column.into() {
+                    render_cell(cell, true, col_chunks[x], buf);
+                } else {
+                    render_cell(cell, false, col_chunks[x], buf);
+                }
+            }
+        }
+    }
+}
+
+fn render_cell(cell: &Cell, display_cursor: bool, area: Rect, buf: &mut Buffer) {
+    let mut style = match cell.cell_type {
+        CellType::Bomb => Style::default().bg(Color::Red).fg(Color::White),
+        CellType::Numbered(_) => Style::default().bg(Color::DarkGray).fg(Color::Yellow),
+        CellType::Empty => Style::default().bg(Color::Black),
+    };
+
+    if display_cursor {
+        style = style.patch(Style::default().reversed());
+    }
+
+    let block = Block::new().style(style).padding(Padding::top(1));
+
+    match cell.cell_type {
+        CellType::Bomb => Paragraph::new("*")
+            .block(block)
+            .centered()
+            .render(area, buf),
+        CellType::Numbered(number) => Paragraph::new(number.to_string())
+            .block(block)
+            .centered()
+            .render(area, buf),
+        CellType::Empty => block.render(area, buf),
+    };
+}
